521874ca6356db4679db0b7bbb1390e1f2dedc6a
Ramiro Gutiérrez Alaniz <gunt_raro@hotmail.com>
2017-06-11 06:26:26 +0000

    Postgresl heroku

--- balpres_be/settings.py
+++ balpres_be/settings.py
@@ -144,12 +144,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 CORS_ORIGIN_ALLOW_ALL = False
 
